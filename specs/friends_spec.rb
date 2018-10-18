8c6716f01c9d2cbcdf397e2f7c113ff404ffc1e3
Catriona Ford <catriona.b.ford@gmail.com>
2018-10-18 16:03:40 +0100

    completed by 4pm

--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -74,27 +74,50 @@ class TestFriends < MiniTest::Test
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
 
+  def test_getting_tv_show
+    result = get_tv_show(@person3)
+    assert_equal("Pokemon", result)
+  end
+
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person2, "bread") should return true, likes_to_eat(@person3, "spinach") should return false)
+
+  def test_likes_to_eat
+    result = likes_to_eat(@person1, "charcuterie")
+    assert_equal(true, result)
+  end
 
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
 
+  def test_add_friend
+    result = add_friend(@person5, "Fred")
+    assert_equal(@person5[:friends].length, result)
+  end
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
 
+def test_remove_friend
+  result = remove_friend(@person1, "Fred")
+  assert_equal(@person1[:friends].length, result)
+end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
 
+def test_total_money
+  result = total_money(@people)
+  assert_equal(143, result)
+end
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
   # (hint: our function will probably need 3 arguments passed to it... the lender, the lendee, and the amount for this function)
   # (hint2: You should test if both the lender's and the lendee's money have changed, maybe two assertions?)
 
+def test_
 
   # 8. Find the set of everyone's favourite food joined together
   # (hint: concatenate the favourites/snack arrays together)
