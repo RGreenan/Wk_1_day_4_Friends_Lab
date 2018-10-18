733c07db4b38be6e149c458ebf7d0c0110f766c6
Catriona Ford <catriona.b.ford@gmail.com>
2018-10-18 16:04:43 +0100

    Everything done by 4pm

--- friends.rb
+++ friends.rb
@@ -1,3 +1,31 @@
 def get_name(person)
   return person[:name]
 end
+
+def get_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_to_eat(person, food)
+  if food == person[:favourites][:snacks][0]
+    return true
+  end
+end
+
+def add_friend(person, friend)
+  person[:friends] << friend
+  person[:friends].length
+end
+
+def remove_friend(person, friend)
+  person[:friends].delete("Fred")
+  return person[:friends].length
+end
+
+def total_money(people)
+  all_the_money = 0
+  for person in people
+    all_the_money += person[:monies]
+  end
+  return all_the_money
+end
